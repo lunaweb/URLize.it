6aa91ae328480bd19f9a4185eb783d4b516c70f5
Kaelig - LunaWeb <kaelig@lunaweb.fr>
2010-09-28 00:35:50 +0200

    Porting changes to the api

--- api/index.php
+++ api/index.php
@@ -10,12 +10,15 @@ function new_urlizisation()
 {
   if (!empty($_POST['dirty_url'])) {
       // retrieve path to urlize in $_POST
-      $dirty_url = rtrim(trim(stripslashes($_POST['dirty_url'])));
+      $dirty_url = rtrim(trim(stripslashes(htmlspecialchars($_POST['dirty_url']))));
+      
+      // Separator : Defaulting to dashes
+      $dashes_or_underscores = !empty($_POST['underscores']) ? '_' : '-';
 
       // TODO: normalize/cleanup/validate data
       $data = array(
         "dirty_url" => $dirty_url,
-        "urlized_data" => file_adresse($dirty_url)
+        "urlized_data" => urlize($dirty_url, true, true, $dashes_or_underscores)
       );
       return json_encode($data); 
   }
